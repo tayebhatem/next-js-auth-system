9e0bf1d45f9c3d928fe121d986c44d8572050a75
tayebhatem <tayebhatem98@gmail.com>
2024-05-06 20:17:16 +0200

    new commit

--- lib/mail.ts
+++ lib/mail.ts
@@ -7,7 +7,7 @@ const domain=process.env.NEXT_PUBLIC_URL
 
 export const sendVerificationEmail=async(email:string,token:string)=>{
    
-    const confirmEmail = `${domain}/auth/new-verification?token=${token}`;
+    const confirmEmail = `"${domain}"/auth/new-verification?token=${token}`;
     await resend.emails.send({
         from:'onboarding@resend.dev',
         to: email,
@@ -16,7 +16,7 @@ export const sendVerificationEmail=async(email:string,token:string)=>{
     })
 }
 export const sendPasswordReset=async(email:string,token:string)=>{
-    const resetLink = `${domain}/auth/new-password?token=${token}`;
+    const resetLink = `"${domain}"/auth/new-password?token=${token}`;
     await resend.emails.send({
         from:'onboarding@resend.dev',
         to: email,
