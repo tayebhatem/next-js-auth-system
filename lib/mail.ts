dd31cbfe126d9d9c8ea3d935826c481acd62b256
tayebhatem <tayebhatem98@gmail.com>
2024-05-06 20:31:29 +0200

    update mail

--- lib/mail.ts
+++ lib/mail.ts
@@ -3,11 +3,11 @@
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.AUTH_RESEND_KEY);
-const domain=process.env.NEXT_PUBLIC_URL
+
 
 export const sendVerificationEmail=async(email:string,token:string)=>{
    
-    const confirmEmail = `"${domain}"/auth/new-verification?token=${token}`;
+    const confirmEmail = `https://next-js-auth-system-ten.vercel.app/auth/new-verification?token=${token}`;
     await resend.emails.send({
         from:'onboarding@resend.dev',
         to: email,
@@ -16,7 +16,7 @@ export const sendVerificationEmail=async(email:string,token:string)=>{
     })
 }
 export const sendPasswordReset=async(email:string,token:string)=>{
-    const resetLink = `"${domain}"/auth/new-password?token=${token}`;
+    const resetLink = `https://next-js-auth-system-ten.vercel.app/auth/new-password?token=${token}`;
     await resend.emails.send({
         from:'onboarding@resend.dev',
         to: email,
